1c15724d2a8da6ef7de25906bb1c85fad2fe8df1
marioperez25 <mariofph@gmail.com>
2020-06-23 16:09:03 -0600

    added support for multiple outputs in largest.js

--- js-modules/largest.js
+++ js-modules/largest.js
@@ -1,8 +1,28 @@
 const largest = function(array){
     let longestString = '';
-    array.forEach(element => element.length >= longestString.length 
-        ? longestString = element : null);
-    return longestString;
+    let longestStrings;
+    let result;
+
+    // filter the largest string possible
+    array.forEach(
+        element => {
+            element.length >= longestString.length 
+            ? longestString = element : null;
+        }
+    );
+
+    // filter all items that match the largest string
+    longestStrings = array.filter( 
+        string => string.length ===  longestString.length
+    );
+    
+    // determine output if single or multiple results
+    let isThereMoreThanOneResult = longestStrings.length >= 2;
+    isThereMoreThanOneResult ?
+        result = longestStrings
+        : result = longestString
+    
+    return result;
 }
 
 module.exports = largest;
--- js-modules/largest.test.js
+++ js-modules/largest.test.js
@@ -1,10 +1,13 @@
 const largest = require('./largest');
 let mockArray1 = ['1' , '12', '123'];
 let mockArray2 = ['100' , '12', '123','testing'];
-let mockArray3 = ['test' , 'data', 'dataTest','long'];
+let mockArray3 = ['test' , 'dataTest', 'dataTest','long'];
 
 test('return the largest string in the array',()=>{
     expect(largest(mockArray1)).toBe('123');
     expect(largest(mockArray2)).toBe('testing');
-    expect(largest(mockArray3)).toBe('dataTest');
+})
+
+test('will return an array of repeated strings with the largest strings', ()=>{
+    expect(largest(mockArray3)).toStrictEqual(['dataTest', 'dataTest']);
 })
